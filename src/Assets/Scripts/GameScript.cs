cfcc94ea70cd3e1eadc48a4b9495becda45262ca
Paul Siicu <psiicu@hotmail.com>
2014-05-14 07:35:53 -0700

    basic explosion and trail code: trails don't work

---
+++ src/Assets/Scripts/GameScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameScript : MonoBehaviour
+{
+	private Dictionary<int, GameObject> trails = new Dictionary<int, GameObject>();
+
+	void Update()
+	{
+		// look for all fingers
+		for (int i = 0; i < Input.touchCount; i++)
+		{
+			Touch touch = Input.GetTouch(i);
+
+			// tap: quick touch & release
+			if (touch.phase == TouchPhase.Ended && touch.tapCount == 1)
+			{
+				// touch are screen locations; convert to world
+				Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+				// effect for feedback
+				SpecialEffectsScript.MakeExplosion((position));
+			}
+
+			// drag
+			if (touch.phase == TouchPhase.Began)
+			{
+				// store this new value
+				if (trails.ContainsKey(i) == false)
+				{
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; // make sure trail is visible
+
+					GameObject trail = SpecialEffectsScript.MakeTrail(position);
+
+					if (trail != null)
+					{
+						Debug.Log(trail);
+						trails.Add(i, trail);
+					}
+				}
+			}
+
+			else if (touch.phase == TouchPhase.Moved)
+			{
+				// move the trail
+				if (trails.ContainsKey(i) == false)
+				{
+					GameObject trail = trails[i];
+
+					Camera.main.ScreenToWorldPoint(touch.position);
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; // make sure trail is visible
+					
+					trail.transform.position = position;
+				}
+			}
+
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				// clear known trails
+				if (trails.ContainsKey(i))
+				{
+					GameObject trail = trails[i];
+
+					// let the trail fade out
+					Destroy (trail, trail.GetComponent<TrailRenderer>().time);
+					trails.Remove (i);
+				}
+			}
+		}
+	}
+}
---
+++ src/Assets/Scripts/SpecialEffectsScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class SpecialEffectsScript : MonoBehaviour
+{
+	private static SpecialEffectsScript instance;
+
+	// prefabs
+	public ParticleSystem explosionEffect;
+	public GameObject trailPrefab;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	void Start()
+	{
+		if (explosionEffect == null)
+			Debug.LogError("Missing explosion effect");
+		if (trailPrefab == null)
+			Debug.LogError("Missing trail prefab");
+	}
+
+	public static ParticleSystem MakeExplosion(Vector3 position)
+	{
+		if (instance == null)
+		{
+			Debug.LogError("There is no SpecialEffectsScript in the scene");
+			return null;
+		}
+
+		ParticleSystem effect = Instantiate(instance.explosionEffect) as ParticleSystem;
+		effect.transform.position = position;
+
+		// program destruction at the end of the effect
+		Destroy (effect.gameObject, effect.duration);
+
+		return effect;
+	}
+
+	public static GameObject MakeTrail(Vector3 position)
+	{
+		if (instance == null)
+		{
+			Debug.LogError("There is no SpecialEffectsScript in the scene");
+			return null;
+		}
+
+		GameObject trail = Instantiate(instance.trailPrefab) as GameObject;
+		trail.transform.position = position;
+
+		return trail;
+	}
+}
